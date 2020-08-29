dbe28faa4941099560540739bf3eb05975b0e4d6
Pritam Maity <pritammaity123456@gmail.com>
2020-08-30 01:52:54 +0530

    images

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Pritam!</h1>');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const myReadStream = fs.createReadStream(__dirname + '/index.html',  'utf8');
+    myReadStream.pipe(res);
+});
+
+app.get('/assets', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const myReadStream = fs.createReadStream(__dirname + '/assets/images',  'utf8');
+    myReadStream.pipe(res);
 });
 
 app.listen(3000,() => {
